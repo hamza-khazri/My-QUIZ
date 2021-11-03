301c3cb975863f23f56bba93cf5088eb94981fc8
hamza-khazri <hamza20251026@gmail.com>
2021-11-03 22:41:47 +0100

    animation
    added header animation

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ async function getip(){
     console.log(quest);
 let element = document.getElementById('question');
 element.innerHTML = quest;
-
     let answors = data[i]["answors"];
     for (var  j in answors){
         console.log(answors[j]);
@@ -21,3 +20,47 @@ document.getElementById('start-btn').addEventListener("click",()=>{
     document.getElementById('quiz').style.display = "inline";
 });
 getip();
+
+
+
+
+
+const typed = document.getElementById("title-text");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["learn","have fun","grow your knowlage"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textI = 0;
+let charI = 0;
+
+function type(){
+    if(charI < textArray[textI].length){
+        if(!cursor.classList.contains("typing")) cursor.classList.add("typing");
+        typed.textContent += textArray[textI].charAt(charI);
+        charI++;
+        setTimeout(type,typingDelay);
+    }else{
+        cursor.classList.remove("typing");
+        setTimeout(erase,newTextDelay);
+    }
+}
+function erase(){
+    if(charI>0){
+        if(!cursor.classList.contains("typing")) cursor.classList.add("typing");
+        typed.textContent = textArray[textI].substring(0,charI-1);
+        charI--;
+        setTimeout(erase,erasingDelay);
+    }else{
+        cursor.classList.remove("typing");
+             textI++;
+             if(textI>=textArray.length) textI=0;
+             setTimeout(type,typingDelay + 1100);
+        }
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    setTimeout(type,newTextDelay + 250);
+});

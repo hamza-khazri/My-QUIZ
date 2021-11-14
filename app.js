4026bf185cfc9a5bd731ec917f26eed5d42ed57c
hamza-khazri <hamza20251026@gmail.com>
2021-11-14 11:25:10 +0100

    added radio
    add radio boxes and fixed the menu hide in wide screens . debuged everything .consol not showing any error

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const api_url = "https://quizapi.io/api/v1/questions?apiKey=YSH1KNbEMiQ1DIKRIQ4pkYIx18oVhLQuEIbAyH8V&category=code&difficulty=Easy&limit=10";
+console.log("appp.js is working");
 
 
+function answersManipulation(array) {
+    // const div = docuement.querySelector('.answers');
+    console.log(array,'answers are working');
+    var counter = 0;
+    var name ='';
+    var div = '';
+    for (j in array){
+        counter++;
+        name = 'l'+ counter;
+        divState = 'd'+ counter;
+        console.log(divState)
+        var l = document.getElementById(name);
+        var box = document.getElementById(counter);
+        var div = document.getElementById(divState);
+        if (array[j] != null){
+            div.style.display="block";
+            l.textContent = array[j];
+            box.setAttribute('value',j);
+        }
+    }
+    console.log("end");
+}
+
+function round(i,data){
+    let quest = data[i]['question'];
+    console.log(quest);
+    let element = document.getElementById('question');
+    element.innerHTML = quest;
+    let answers = data[i]["answers"];
+    answersManipulation(answers);
+}
 
 async function getip(){
     const  response = await fetch(api_url);
     const data = await response.json();
-    console.log(data);
+    console.log(data,"data is working");
     var i = 0;
-    let quest = data[i]['question'];
-    console.log(quest);
-        let element = document.getElementById('question');
-        element.innerHTML = quest;
-    let answers = data[i]["answers"];
-    //     let answerDiv = document.querySelector(".answers");
-    // answers.map(answers =>{
-    //     retrun (<div>{answers}</div>);
-    // })
+    document.getElementById('start-btn').addEventListener("click",()=>{
+        round(i,data);
+        i++;
+    });
+    document.querySelector('.answer-btn').addEventListener("click",()=>{
+        round(i,data);
+        i++;
+    });
 }
+
+
+
 
 getip();
